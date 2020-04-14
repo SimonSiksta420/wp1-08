aa9258a5ccaede8ca586fc6c0e804935d2c8ef42
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-04-14 09:48:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$guessnumber = 55;
+$yournumber = 0;
+define ('MAX_NUMBER', 100);
+define ('MIN_NUMBER', 0);
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p> Hádej číslo mezi <?= MIN_NUMBER ?> - <?= MAX_NUMBER ?> </p>
+<!--<p> Hádám, že je číslo: <?= $yournumber ?> </p>-->
+
+<?php
+
+/*
+if ($guessnumber == $yournumber) {
+    You guessed the number good job
+} else {
+    You missed 
+}
+*/
+
+while ($guessnumber != $yournumber) {
+    Hádané číslo <?= $yournumber ?> není správné. Zkus jiné
+}
+$yournumber = rand(MIN_NUMBER, MAX_NUMBER);
+
+<p> Gratulujeme, uhádl jsi číslo <?= $guessnumber ?> svým hádaným číslem <?= $yournumber ?>
+
+</body>
+</html>
